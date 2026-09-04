108a1d0f9dd5ef0087c2921a80063a183af7f275
heebaek <heebaek@naver.com>
2026-09-04 12:17:42 +0900

    #12 주석 추가, 전체 모바일 화면 반응 추가

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 // TODO: 개발 중 대시보드 확인을 위한 임시 기능입니다.
 // 실제 화면 전환 기능이 완성되면 삭제합니다.
 
+// 아래 주소로 대시보드가 표시된 상태의 페이지로 입장할 수 있습니다.
+// http://127.0.0.1:5500/index.html?view=dashboard
+
 const params = new URLSearchParams(window.location.search);
 const view = params.get("view");
 
